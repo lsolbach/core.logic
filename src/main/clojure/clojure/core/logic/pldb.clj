a86578dd05ebc2376a0fac59b5058aa91d642e21
Ludger Solbach <soulman@soulspace.org>
2023-12-04 10:36:42 +0100

    added some docstrings

--- src/main/clojure/clojure/core/logic/pldb.clj
+++ src/main/clojure/clojure/core/logic/pldb.clj
@@ -7,6 +7,9 @@
 ;   You must not remove this notice, or any other, from this software.
 
 (ns clojure.core.logic.pldb
+  "A persistent core.logic database.
+   The goal of pldb is to provide core.logic fact/relation mechanism that doesn't use mutable namespace references.
+   This makes it easier to use core.logic in multi-threaded environments like web applications."
   (:refer-clojure :exclude [indexed?])
   (:require [clojure.core.logic :as l]))
 
@@ -14,47 +17,68 @@
 
 (def empty-db {})
 
-(defmacro with-dbs [dbs & body]
+(defmacro with-dbs
+  "Creates bindings for the given `dbs` and executes the `body` in the context of these bindings."
+  [dbs & body]
   `(binding [l/*logic-dbs* (concat l/*logic-dbs* ~dbs)]
           ~@body))
 
-(defmacro with-db [db & body]
+(defmacro with-db
+  "Creates a binding for the given `db` and executes the `body` in the context of this binding."
+  [db & body]
   `(binding [l/*logic-dbs* (conj l/*logic-dbs* ~db)]
           ~@body))
 
-(defn facts-for [dbs kname]
+(defn facts-for
+  "Returns the facts from the `dbs` with the key `kname`."
+  [dbs kname]
   (mapcat #(get-in % [kname ::unindexed]) dbs))
 
-(defn facts-using-index [dbs kname index val]
+(defn facts-using-index
+  "Returns the facts from the `dbs` with the key `kname` using the `index` and `val` for lookup."
+  [dbs kname index val]
   (mapcat #(get-in % [kname index val]) dbs))
 
 ;; ----------------------------------------
-(defn rel-key [rel]
+(defn rel-key
+  "Returns the key for the relation `rel`."
+  [rel]
   (if (keyword? rel)
     rel
     (:rel-name (meta rel))))
 
-(defn rel-indexes [rel]
+(defn rel-indexes
+  "Returns the indexes defined on the relation `rel`."
+  [rel]
   (:indexes (meta rel)))
 
-(defn indexed? [v]
+(defn indexed?
+  "Returns true if `v` is indexed."
+  [v]
   (true? (:index (meta v))))
 
 
-(defn contains-lvar? [x]
+(defn contains-lvar?
+  "Returns true if `x` contains logic variables (lvars)."
+  [x]
   (some l/lvar? (tree-seq coll? seq x)))
 
-(defn ground? [s term]
+(defn ground?
+  "Returns true if `x` contains logic variables (lvars)."
+  [s term]
   (not (contains-lvar? (l/walk* s term))))
 
-(defn index-for-query [s q indexes]
+(defn index-for-query
+  [s q indexes]
   (let [indexable (map #(ground? s %)  q)
         triples (map vector (range) indexable indexes)]
     (first (for [[i indexable indexed] triples
                  :when (and indexable indexed)]
              i))))
 
-(defmacro db-rel [name & args]
+(defmacro db-rel
+  "Refines a relation with `name` and the given `args`."
+  [name & args]
   (let [arity
         (count args)
 
@@ -85,7 +109,9 @@
 
 ;; ----------------------------------------
 
-(defn db-fact [db rel & args]
+(defn db-fact
+  "Returns a new db by adding to the given `db` the fact of relation `rel` with the `args`."
+  [db rel & args]
   (let [key
         (rel-key rel)
 
@@ -106,7 +132,9 @@
             db))]
     (reduce update-index-fn db-with-fact indexes-to-update)))
 
-(defn db-retraction [db rel & args]
+(defn db-retraction
+  "Retracts the relation `rel` from the database `db`."
+  [db rel & args]
   (let [key
         (rel-key rel)
 
@@ -128,13 +156,16 @@
     (reduce remove-from-index-fn db-without-fact indexes-to-update)))
 
 ;; ----------------------------------------
-(defn db-facts [base-db & facts]
+(defn db-facts
+  "Returns a facts database based on `base-db` with the given `facts` added."
+  [base-db & facts]
   (reduce #(apply db-fact %1 %2) base-db facts))
 
 (defn db [& facts]
+  "Returns a new facts database with the given `facts` added."
   (apply db-facts empty-db facts))
 
 (defn db-retractions [base-db & retractions]
+  "Returns a database based on `base-db` with the given `retractions` removed."
   (reduce #(apply db-retraction %1 %2) base-db retractions))
-
 
